6e45fcacb21542da7f789339fa22b8a825dc583f
gil <gil.lopez.93i@gmail.com>
2016-03-27 20:55:19 -0600

    eventos

--- app/lib/Audio.js
+++ app/lib/Audio.js
@@ -1,5 +1,6 @@
-//var _                 = require("alloy/underscore")._;
-//svar Collection = require("Collection");
+var _             = require("alloy/underscore")._;
+//var Backbone    = require("alloy/Backbone");
+//var Collection  = require("Collection");
 var stepsVolume = [1,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.09,0.08,0.07,0.06,0.05,0.04,0.03,0.02,0.01,0];
 var total       = stepsVolume.length;
 var A           = null;
@@ -9,8 +10,10 @@ function Audio(){
 	this.active  = null;
     this.index   = 0;
 	this.List    = [];
+	this.complete();
 	A = this;
 }
+_.extend( Audio,A.player );
 Audio.prototype.increment = function(callback){
    callback = callback || function(){};
    var audio = this.player; 
@@ -94,6 +97,23 @@ Audio.prototype.play = function( index  ){
     this.active = this.List[ this.index ];
     this.player.setUrl( this.active );
     this.player.play();
+    console.log( this.player );
+};
+Audio.prototype.pause = function( deg ){
+	deg = deg || false;
+    if( deg ){
+		this.decrement(function(){
+	    	A.player.pause();
+	    });
+	}else{
+		this.player.pause();
+	}
+};
+Audio.prototype.complete = function(){
+	this.player.addEventListener("complete",function(){
+		A.reset();
+		A.next();
+	});
 };
 Audio.prototype.stop = function( index  ){
     this.reset();
@@ -104,8 +124,15 @@ Audio.prototype.setList = function( List ){
 Audio.prototype.getList = function(){
     return this.List;
 };
-Audio.prototype.getCurrentName = function(){
-    return this.player;
+Audio.prototype.getIndex = function(){
+    return this.index;
+};
+Audio.prototype.getInfo = function(){
+    return {
+    	name : A.player.url.match(/([^\/]+)(?=\.\w+$)/)[0],
+    	duration : A.player.duration,
+    	currentTime : A.player.time
+    };
 };
 Audio.prototype.getCurrentAudio = function(){
     return this.active;
